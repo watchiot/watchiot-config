21fe56aa856cd0dcb375d1ef7d850b38dc64609a
gorums <acksecurity@hotmail.com>
2016-04-18 00:14:38 -0500

    Add router for get the config.yaml, readme.md and the svg.

--- routes/repos.js
+++ routes/repos.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var repos = require('../repos/repos.json');
+var path = require('path');
+var fs = require('fs');
 
 router.get('/', function(req, res, next) {
   var lst_repos = {};
@@ -12,5 +14,30 @@ router.get('/', function(req, res, next) {
 
   res.json(lst_repos);
 });
+
+/* router for config.yaml */
+for (var index in repos.repos) {
+  var repo_folder = repos.repos[index];
+  router.get('/' + repo_folder + '/config.yaml', function (req, res, next) {
+    res.send(req.url);
+  });
+}
+
+/* router for readme.md */
+for (var index in repos.repos) {
+  var repo_folder = repos.repos[index];
+  router.get('/' + repo_folder + '/readme.md', function (req, res, next) {
+    res.send(req.url);
+  });
+}
+
+ /* router for svg */
+for (var index in repos.repos) {
+  var repo_folder = repos.repos[index];
+  var repo = require('../repos/' + repo_folder + '/repo.json');
+  router.get('/' + repo_folder + '/' + repo.repo.image, function (req, res, next) {
+    res.sendFile(path.resolve('../repos/' + req.url));
+  });
+}
 
 module.exports = router;
