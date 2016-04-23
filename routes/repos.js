c051dbed1e9f9e117449862cca57d6c98ef0b422
gorums <acksecurity@hotmail.com>
2016-04-23 01:45:17 -0500

    delete path module

--- routes/repos.js
+++ routes/repos.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 var fs = require('fs');
-var path = require('path');
 
 /* metadata about the repos */
 var repos = require('../repos/repos.json');
